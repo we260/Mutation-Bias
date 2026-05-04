b95d7d41f5fd733436e56e055a5a1f8f25d6e11b
we260 <we260@bath.ac.uk>
2026-05-04 17:47:44 +0100

    Update Singleton_bar_charts.r

--- Singleton_bar_charts.r
+++ Singleton_bar_charts.r
@@ -1,6 +1,6 @@
 library(ggplot2)
 
-base_dir <- "C:/Users/willi/OneDrive/Documents/University/Year 3/Semester 2/Capstone/Designated Species"
+base_dir <- r'C:\Users\[USER]\...\Species_folder'
 out_dir <- file.path(base_dir, "Final Figures")
 
 df <- read.csv(file.path(base_dir, "master_species_data.csv"))
